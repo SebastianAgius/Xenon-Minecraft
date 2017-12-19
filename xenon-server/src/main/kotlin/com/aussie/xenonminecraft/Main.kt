fd119302e28147a562bd532da5ba812108434efb
BuildTools <unconfigured@null.spigotmc.org>
2017-12-19 22:07:16 +1100

    Start working on server & remove natives

---
+++ xenon-server/src/main/kotlin/com/aussie/xenonminecraft/Main.kt
@@ -0,0 +1,14 @@
+package com.aussie.xenonminecraft
+
+
+fun main(args: Array<String>) {
+
+}
+
+class Main {
+    companion object {
+        val DEBUG = true
+
+        val NATIVE_LIB = "xms"
+    }
+}
